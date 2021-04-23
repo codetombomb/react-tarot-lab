b77ccfd975c40cb49f6838e8617411e3a911e842
Tom Tobar <codetombomb@gmail.com>
2021-04-22 21:43:05 -0500

    add card components and style.

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render() {
+        return(
+            <div className="card-wrapper">
+                <div className="card" >
+                    {/* <p>{this.props.info.name}</p> */}
+                </div>
+            </div>
+        )
+    }
+}
+export default Card;
